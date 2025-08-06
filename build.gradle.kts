640a5faa911c53d83b1f70cbb9ce3385682bd050
jingyu <wlsrb97@naver.com>
2025-08-07 00:33:05 +0900

    test: UserServiceTest 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     java
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "com.example"
@@ -10,6 +15,8 @@ version = "0.0.1-SNAPSHOT"
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
@@ -64,11 +71,42 @@ dependencies {
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
     annotationProcessor("jakarta.annotation:jakarta.annotation-api")
 
+    //test
+    testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.mockk:mockk:1.13.12")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+    testImplementation("io.kotest:kotest-framework-engine:5.8.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
-tasks.test {
+kotlin{
+    sourceSets {
+        test {
+            kotlin.srcDirs(listOf("src/test/kotlin"))
+        }
+    }
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
+    jvmArgs("--enable-preview")
+}
+
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.addAll(listOf(
+        "--enable-preview",
+        "-Amapstruct.defaultComponentModel=spring",
+    ))
+}
+
+tasks.named<JavaExec>("bootRun") {
+    jvmArgs("--enable-preview")
 }
