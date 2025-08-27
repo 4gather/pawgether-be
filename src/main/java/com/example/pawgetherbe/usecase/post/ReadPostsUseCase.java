7ff02f628cd2f48de9d6ef289ad8eaa29ce7aa7a
jingyu <wlsrb97@naver.com>
2025-08-28 00:33:11 +0900

    feat(petFair): main 페이지 Carousel 조회 구현

--- src/main/java/com/example/pawgetherbe/usecase/post/ReadPostsUseCase.java
+++ src/main/java/com/example/pawgetherbe/usecase/post/ReadPostsUseCase.java
@@ -1,4 +1,7 @@
 package com.example.pawgetherbe.usecase.post;
 
+import com.example.pawgetherbe.controller.query.dto.PetFairQueryDto.PetFairCarouselResponse;
+
 public interface ReadPostsUseCase {
+    PetFairCarouselResponse petFairCarousel();
 }
