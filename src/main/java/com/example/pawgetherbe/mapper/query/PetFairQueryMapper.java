775958ce291bea21c8234a22b6228d8e56b7fc0d
jingyu <wlsrb97@naver.com>
2025-09-10 23:21:55 +0900

    refactor(PetFair): PetFairQueryMapper 에  Entity -> Dto 로 변환 시 @Mapping(target = "petFairId", source = "id") 추가

--- src/main/java/com/example/pawgetherbe/mapper/query/PetFairQueryMapper.java
+++ src/main/java/com/example/pawgetherbe/mapper/query/PetFairQueryMapper.java
@@ -4,10 +4,13 @@ import com.example.pawgetherbe.controller.query.dto.PetFairQueryDto.DetailPetFai
 import com.example.pawgetherbe.controller.query.dto.PetFairQueryDto.SummaryPetFairResponse;
 import com.example.pawgetherbe.domain.entity.PetFairEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface PetFairQueryMapper {
-
+    @Mapping(target = "petFairId", source = "id")
     DetailPetFairResponse toDetailPetFair(PetFairEntity petFairEntity);
+
+    @Mapping(target = "petFairId", source = "id")
     SummaryPetFairResponse toSummaryPetFair(PetFairEntity entity);
 }
