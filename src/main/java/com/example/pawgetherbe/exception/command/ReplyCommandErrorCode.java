dc0ed2b950afbf03179a8996393ba828ea74b565
jingyu <wlsrb97@naver.com>
2025-09-21 17:39:12 +0900

    refactor(Reply): Reply 생성, 삭제 시 에러코드 추가

--- src/main/java/com/example/pawgetherbe/exception/command/ReplyCommandErrorCode.java
+++ src/main/java/com/example/pawgetherbe/exception/command/ReplyCommandErrorCode.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public enum ReplyCommandErrorCode implements ErrorCode {
     NOT_FOUND_REPLY(HttpStatus.NOT_FOUND, "NOT_FOUND_REPLY", "답글이 존재하지 않습니다."),
+    CREATE_INTERNAL_REPLY(HttpStatus.INTERNAL_SERVER_ERROR, "CREATE_INTERNAL_REPLY", "답글 생성중 오류가 발생했습니다."),
+    DELETE_CONFLICT_REPLY(HttpStatus.CONFLICT, "DELETE_CONFLICT_REPLY", "이미 삭제된 답글입니다."),
     ;
 
     private final String message;
--- src/main/java/com/example/pawgetherbe/service/command/ReplyCommandService.java
+++ src/main/java/com/example/pawgetherbe/service/command/ReplyCommandService.java
@@ -1,10 +1,10 @@
 package com.example.pawgetherbe.service.command;
 
 import com.example.pawgetherbe.common.exceptionHandler.CustomException;
-import com.example.pawgetherbe.controller.command.dto.ReplyCommandDto.ReplyUpdateResponse;
-import com.example.pawgetherbe.controller.command.dto.ReplyCommandDto.ReplyUpdateRequest;
 import com.example.pawgetherbe.controller.command.dto.ReplyCommandDto.ReplyCreateRequest;
 import com.example.pawgetherbe.controller.command.dto.ReplyCommandDto.ReplyCreateResponse;
+import com.example.pawgetherbe.controller.command.dto.ReplyCommandDto.ReplyUpdateRequest;
+import com.example.pawgetherbe.controller.command.dto.ReplyCommandDto.ReplyUpdateResponse;
 import com.example.pawgetherbe.domain.status.ReplyStatus;
 import com.example.pawgetherbe.mapper.command.ReplyCommandMapper;
 import com.example.pawgetherbe.repository.command.CommentCommandRepository;
@@ -20,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static com.example.pawgetherbe.domain.UserContext.getUserId;
 import static com.example.pawgetherbe.exception.command.CommentCommandErrorCode.NOT_FOUND_COMMENT;
+import static com.example.pawgetherbe.exception.command.ReplyCommandErrorCode.CREATE_INTERNAL_REPLY;
+import static com.example.pawgetherbe.exception.command.ReplyCommandErrorCode.DELETE_CONFLICT_REPLY;
 import static com.example.pawgetherbe.exception.command.ReplyCommandErrorCode.NOT_FOUND_REPLY;
 import static com.example.pawgetherbe.exception.command.UserCommandErrorCode.NOT_FOUND_USER;
 
@@ -49,9 +51,13 @@ public class ReplyCommandService implements RegistryReplyUseCase, EditReplyUseCa
                 .status(ReplyStatus.ACTIVE)
                 .build();
 
-        var reply = replyCommandRepository.save(replyEntity);
-
-        return replyCommandMapper.toReplyCreateResponse(reply);
+        try{
+            var reply = replyCommandRepository.save(replyEntity);
+            return replyCommandMapper.toReplyCreateResponse(reply);
+        } catch (Exception e){
+            log.error(e.getMessage());
+            throw new CustomException(CREATE_INTERNAL_REPLY);
+        }
     }
 
     @Override
@@ -80,7 +86,9 @@ public class ReplyCommandService implements RegistryReplyUseCase, EditReplyUseCa
         assertCommentExists(commentId);
 
         var replyEntity = replyCommandRepository.findById(replyId).orElseThrow(() -> new CustomException(NOT_FOUND_REPLY));
-
+        if(replyEntity.getStatus().equals(ReplyStatus.REMOVED)){
+            throw new CustomException(DELETE_CONFLICT_REPLY);
+        }
         replyEntity.updateStatus(ReplyStatus.REMOVED);
     }
 
